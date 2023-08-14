87ef37ea03c7eb9ad453d19917a4ed9ad76a1d43
KottaiRajAit <119566130+KottaiRajAit@users.noreply.github.com>
2023-08-14 23:05:11 +0530

    Add files via upload

---
+++ public/ffr-track.js
@@ -0,0 +1,44 @@
+
+/**
+ * Set the API key
+ * @param {string} ochnkey - The API key
+ */
+
+let ochnkey = 'jtxkkvu39rkc3wuy4s6xns1z6';
+
+(function (d, s, id) {
+    let js, ojs = d.getElementsByTagName(s)[0];
+
+    if (d.getElementById(id)) { return; }
+
+    js = d.createElement(s); js.id = id; js.async = !0;
+
+    js.src = "http://localhost:3000/jsapi/omnichannel.js";
+
+    ojs.parentNode.insertBefore(js, ojs);
+
+}(document, 'script', 'omnichanneltrack'));
+
+/**
+ * Initialization function 
+ * ocnh.init()   - Initialized the F-AI omnichannel Js library
+ * ochn.send()   - Tracking the search of the page
+ */
+async function ochntrack() {
+    if (ochn.init) ochn.init();
+    if (ochn.send) ochn.send("action", "visit");
+    let siteUrl = window.location.href
+    const queryString = siteUrl.split('?')[1];
+    const queryParams = queryString.split('&');
+    const params = {};
+    queryParams.forEach(param => {
+      const [key, value] = param.split('=');
+      params[key] = value;
+  });
+
+    const email = params.email;
+    let body = {email:email}
+    let populatedData = ochn.prepopulate(body);
+    const event = new CustomEvent('populatedData', { detail: populatedData });
+    window.dispatchEvent(event);
+}
